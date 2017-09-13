7f450d8cc8b7f841a89df8e5602750ed7be32433
Denis Borzenko <bbars@li.ru>
2017-09-13 15:58:35 +0300

    LoopedArray: initial

---
+++ js-looped-array/looped-array.js
@@ -0,0 +1,57 @@
+function ConfinedArray(length) {
+	var cursor = 0;
+	this.length = length || 0;
+	
+	Object.defineProperties(this, {
+		cursor: {
+			get: function () {
+				return cursor;
+			},
+		},
+		push: {
+			value: function (value) {
+				if (!this.length)
+					throw new Error("Buffer underflow");
+				return this[cursor++ % this.length] = value;
+			},
+		},
+		unshift: {
+			value: function (value) {
+				if (!this.length)
+					throw new Error("Buffer underflow");
+				cursor--;
+				if (cursor < 0)
+					cursor += this.length;
+				return this[cursor % this.length] = value;
+			},
+		},
+	});
+}
+
+ConfinedArray.prototype = Object.create(Array.prototype);
+ConfinedArray.prototype.constructor = ConfinedArray;
+
+ConfinedArray.prototype.getIterator = function () {
+	var cursor = this.cursor;
+	var index = -1;
+	var _this = this;
+	
+	return {
+		next: function () {
+			if (index == _this.length - 1)
+				return null;
+			index++;
+			return {
+				index: index,
+				cursor: cursor,
+				position: cursor % _this.length,
+				value: _this[cursor++ % _this.length],
+			};
+		},
+	};
+};
+
+ConfinedArray.prototype.toArray = function () {
+	var position = this.cursor % this.length;
+	return this.slice(position).concat(this.slice(0, position));
+};
