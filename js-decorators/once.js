5d22c8588343da695ae1cba84f3e382f3d43b83f
Denis Borzenko <bbars@li.ru>
2017-09-28 16:59:08 +0300

    Add function.abort method to cancel delayed once-call; refactor a bit

--- js-decorators/once.js
+++ js-decorators/once.js
@@ -1,5 +1,7 @@
 Function.prototype.once = function once(period, context, args) {
 	var now = (new Date()).getTime();
+	if (typeof this != 'function')
+		throw new Error('Function.once() should be called within a function scope');
 	if (!this.__once__) {
 		Object.defineProperty(this, '__once__', {
 			value: {
@@ -10,23 +12,42 @@ Function.prototype.once = function once(period, context, args) {
 				timeout: null,
 				period: -1,
 				promise: null,
+				abort: false,
+				abortReason: null,
 			},
 		});
 	}
 	var once = this.__once__;
 	once.context = context;
 	once.args = Array.prototype.slice.call(arguments, 2);
-	if (!once.timeout || once.period <= 0 || once.period > period) {
+	if (!once.timeout || period < once.period || once.period <= 0) {
 		once.period = period;
-		once.promise = new Promise(function (resolve) {
+		once.promise = new Promise(function (resolve, reject) {
 			once.timeout && clearTimeout(once.timeout);
 			once.timeout = setTimeout(function () {
 				once.timeout = null;
-				resolve(once.fn.apply(once.context, once.args));
+				if (!once.abort) {
+					try {
+						resolve(once.fn.apply(once.context, once.args));
+					}
+					catch (e) {
+						reject(e);
+					}
+				}
+				else if (once.abortReason)
+					reject(once.abortReason);
 			}, once.period);
 		});
 		once.time = now;
 	}
 	
 	return once.promise;
+};
+
+Function.prototype.abort = function abort(reason) {
+	if (!this.__once__ || !this.__once__.timeout)
+		return false;
+	this.__once__.abort = true;
+	this.__once__.abortReason = reason;
+	return true;
 };
