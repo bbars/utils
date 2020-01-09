93a73a44c7ee6d4f956c59ee147fd77a721ddcbe
Denis Borzenko <d.borzenko@hpmd.ru>
2020-01-09 14:24:13 +0300

    Curl: add setConnectTimeout, setCookieJar, setSslVerification, setProxy, getProxy, close, findResponseHeader, getError; Curl::execMulti; describe exceptions; getResponseBody now decodes body according to the Content-Encoding response header

--- php-curl/Curl.php
+++ php-curl/Curl.php
@@ -6,6 +6,8 @@ class Curl {
 	private $addedHeaders = [];
 	protected $method = 'GET';
 	protected $url = '';
+	protected $proxy = '';
+	protected $sslVerification = true;
 	
 	public function __construct($method, $url) {
 		$this->ch = curl_init();
@@ -14,8 +16,9 @@ class Curl {
 	}
 	
 	public static function buildUrl($url, $add = null) {
-		if (!is_array($url))
+		if (!is_array($url)) {
 			$url = parse_url($url);
+		}
 		
 		if (!empty($add)) {
 			if (!is_array($add))
@@ -93,6 +96,10 @@ class Curl {
 		return $this->setOpt(CURLOPT_TIMEOUT_MS, $valueMs);
 	}
 	
+	public function setConnectTimeout($valueMs) {
+		return $this->setOpt(CURLOPT_CONNECTTIMEOUT_MS, $valueMs);
+	}
+	
 	public function setRequestMethod($value) {
 		return $this->setOpt(CURLOPT_CUSTOMREQUEST , $value);
 	}
@@ -103,6 +110,76 @@ class Curl {
 	
 	public function setUrl($value) {
 		return $this->setOpt(CURLOPT_URL, $value);
+	}
+	
+	public function setCookieJar($cookieJarFile) {
+		return $this->setOpt(array(
+			CURLOPT_COOKIEJAR => $cookieJarFile,
+			CURLOPT_COOKIEFILE => $cookieJarFile,
+		));
+	}
+	
+	public function setSslVerification($value) {
+		$this->sslVerification = !!$value;
+		return $this->setOpt(array(
+			CURLOPT_SSL_VERIFYHOST => !!$value,
+			CURLOPT_SSL_VERIFYPEER => !!$value,
+		));
+	}
+	
+	public function setProxy($value) {
+		$proxyHost = null;
+		$proxyPort = null;
+		$proxyType = null;
+		$proxyAuth = null;
+		$proxyTunnel = null;
+		if ($value) {
+			$value = (preg_match('/^[^:]+:\/\//', $value) ? '' : 'http://') . $value;
+			$parts = parse_url($value);
+			
+			$proxyHost = $parts['host'];
+			$proxyPort = $parts['port'];
+			if (empty($parts['port'])) {
+				throw new InvalidArgumentException("Invalid proxy value: port was not set");
+			}
+			
+			$proxyType = CURLPROXY_HTTP;
+			if ($parts['scheme'] === 'socks' || $parts['scheme'] === 'socks5') {
+				$proxyType = CURLPROXY_SOCKS5;
+			}
+			else if ($parts['scheme'] === 'socks4') {
+				$proxyType = CURLPROXY_SOCKS4;
+			}
+			else if ($parts['scheme'] === 'http' || $parts['scheme'] === 'https') {
+				$proxyType = CURLPROXY_HTTP;
+			}
+			else {
+				throw new InvalidArgumentException("Invalid proxy value: unsupported proxy type '{$parts['scheme']}'");
+			}
+			
+			$proxyAuth = '';
+			if (!empty($parts['user'])) {
+				$proxyAuth = $parts['user'] . ':' . (empty($parts['pass']) ? '' : $parts['pass']);
+			}
+			
+			$params = array();
+			if (!empty($parts['query'])) {
+				parse_str($parts['query'], $params);
+			}
+			$proxyTunnel = !empty($params['tunnel']);
+		}
+		$this->proxy = $value;
+		return $this->setOpt(array(
+			CURLOPT_PROXY => $proxyHost,
+			CURLOPT_PROXYPORT => $proxyPort,
+			CURLOPT_PROXYTYPE => $proxyType,
+			CURLOPT_PROXYUSERPWD => $proxyAuth,
+			CURLOPT_HTTPPROXYTUNNEL => $proxyTunnel,
+		));
+	}
+	
+	public function getProxy() {
+		return $this->proxy;
 	}
 	
 	public function getUrl() {
@@ -124,14 +201,15 @@ class Curl {
 	}
 	
 	public function setBody($value, $contentType = null) {
-		$this->setOpt(CURLOPT_POSTFIELDS, $value);
-		if ($contentType)
+		if ($contentType) {
 			$this->setHeader("Content-Type: $contentType");
+		}
+		$this->setOpt(CURLOPT_POSTFIELDS, $value);
 		return $this;
 	}
 	
 	public function setPayload($value, $contentType = null) {
-		if ($this->method == 'POST' || $this->method == 'PUT') {
+		if ($this->method === 'POST' || $this->method === 'PUT') {
 			$this->setBody($value, $contentType);
 		}
 		else {
@@ -152,14 +230,35 @@ class Curl {
 		return $this;
 	}
 	
-	public function send() {
+	protected function onBeforeExec() {
 		$this->setOpt(array(
 			CURLOPT_HEADER => true,
 			CURLINFO_HEADER_OUT => true,
 			CURLOPT_RETURNTRANSFER => true,
 		));
-		$this->response = self::parseHttpResponse(curl_exec($this->ch));
+	}
+	
+	protected function onAfterExec($response) {
+		$this->response = self::parseHttpResponse($response);
+	}
+	
+	public function exec() {
+		$this->onBeforeExec();
+		$this->onAfterExec(curl_exec($this->ch));
 		return $this;
+	}
+	
+	public function send() {
+		return $this->exec();
+	}
+	
+	public function close() {
+		$success = false;
+		if ($this->ch) {
+			$success = curl_close($this->ch);
+			$this->ch = null;
+		}
+		return $success;
 	}
 	
 	protected static function parseHttpResponse($s) {
@@ -174,12 +273,49 @@ class Curl {
 		return !empty($this->response[0]) ? array_filter(explode("\r\n", $this->response[0])) : null;
 	}
 	
+	public function findResponseHeader($name, $first = false) {
+		$headers = $this->getResponseHeaders();
+		if (empty($headers)) {
+			return null;
+		}
+		if ($name === null) {
+			return $headers[0];
+		}
+		$name = $name === null ? $name : strtolower($name);
+		$res = [];
+		foreach ($headers as $header) {
+			$header = explode(':', $header, 2);
+			if (count($header) !== 2) {
+				continue;
+			}
+			if (strtolower($header[0]) === $name) {
+				if ($first) {
+					return trim($header[1]);
+				}
+				$res[] = trim($header[1]);
+			}
+		}
+		return $res;
+	}
+	
 	public function getResponseBody() {
-		return isset($this->response[1]) ? $this->response[1] : null;
+		if (!isset($this->response[1])) {
+			return null;
+		}
+		$body = $this->response[1];
+		$contentEncoding = $this->findResponseHeader('Content-Encoding', true);
+		if ($contentEncoding === 'gzip') {
+			$body = gzdecode($body);
+		}
+		else if ($contentEncoding === 'deflate') {
+			$body = gzinflate($body);
+		}
+		return $body;
 	}
 	
 	public function getResponseJson() {
-		return isset($this->response[1]) ? json_decode($this->response[1]) : null;
+		$body = $this->getResponseBody();
+		return $body === null ? null : json_decode($body);
 	}
 	
 	public function getInfo() {
@@ -188,5 +324,71 @@ class Curl {
 	
 	public function getCode() {
 		return curl_getinfo($this->ch, CURLINFO_RESPONSE_CODE);
+	}
+	
+	public function getError() {
+		$errno = curl_errno($this->ch);
+		if ($errno) {
+			return new CurlException(curl_error($this->ch), $errno, $this);
+		}
+		$httpCode = $this->getCode();
+		if ($httpCode === 404) {
+			return new CurlHttpError404("Not Found", $this);
+		}
+		if (200 > $httpCode || $httpCode >= 300) {
+			return new CurlHttpError("HTTP Error $httpCode", $this);
+		}
+		return null;
+	}
+	
+	///////////////////////
+	
+	public static function execMulti(array $curls) {
+		$mh = curl_multi_init();
+		
+		foreach ($curls as $curl) {
+			if (!is_a($curl, CLASS)) {
+				continue;
+			}
+			curl_multi_add_handle($mh, $curl->ch);
+			$curl->onBeforeExec();
+		}
+		
+		do {
+			$status = curl_multi_exec($mh, $active);
+			if ($active) {
+				curl_multi_select($mh);
+			}
+		} while ($active && $status == CURLM_OK);
+		
+		foreach ($curls as $curl) {
+			$curl->onAfterExec(curl_multi_getcontent($curl->ch));
+			if (!is_a($curl, CLASS)) {
+				continue;
+			}
+		}
+		
+		curl_multi_close($mh);
+	}
+}
+
+class CurlException extends Exception {
+	public $c = null;
+	
+	public function construct($message, $code, Curl $c = null) {
+		parent::construct($message, $code);
+		$this->c = $c;
+	}
+}
+
+class CurlHttpError extends CurlException {
+	public function construct($message, Curl $c = null) {
+		parent::construct($message, $c === null ? -1 : $c->getCode(), $c);
+	}
+}
+
+class CurlHttpError404 extends CurlHttpError {
+	public function construct($message, Curl $c = null) {
+		parent::construct($message, $c);
 	}
 }
